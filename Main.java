fbf7430706224b103f7e61f2c1519ab57a13e36c
Satyaki7 <dsatyaki956@gmail.com>
2025-05-04 22:04:39 +0530

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Fun stuff
+        // Date dateop = new Date(); <--- 4/5/2025
+        // System.out.println(dateop);
+        // System.out.println("working");
+        //Arrays
+        // int[] num = new int[5]; // initialising an array
+        // num[0] = 1;
+        // num[1] = 100;
+        // //Arrays.toString(num) <--- this function gets the the values of a array and converts them to a string to be printed.
+        // System.out.println(Arrays.toString(nums));
+    }
+}
