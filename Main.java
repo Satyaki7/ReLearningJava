9362e9c6da6f50ac3094af9304386b6fd015d069
Satyaki7 <dsatyaki956@gmail.com>
2025-05-07 19:57:28 +0530

    2nd commit

--- Main.java
+++ Main.java
@@ -1,18 +1,27 @@
 
-
 public class Main {
 
     public static void main(String[] args) {
 
-        //Fun stuff
+        //-------------Fun stuff-----------------------
         // Date dateop = new Date(); <--- 4/5/2025
         // System.out.println(dateop);
         // System.out.println("working");
-        //Arrays
+        //
+        //----------------Arrays------------------
         // int[] num = new int[5]; // initialising an array
         // num[0] = 1;
         // num[1] = 100;
         // //Arrays.toString(num) <--- this function gets the the values of a array and converts them to a string to be printed.
         // System.out.println(Arrays.toString(nums));
+        // int[] nums = {1, 21, 55}; // with variables
+        // System.out.println(nums.length);
+        // 
+        //---------------------Two Dimensional Arrays-----------------------
+        // int[][] nums = new int[2][3]; // two rows and 3 columns for more dimensions add more []
+        // nums[0][0] = 1;
+        // System.out.println(Arrays.deepToString(nums)); // Arrays.deepToString() for multidimensional arrays
+        // int[][] nums = {{1, 2, 3}, {5, 6, 7}};
+        // System.out.println(Arrays.deepToString(nums));
     }
 }
