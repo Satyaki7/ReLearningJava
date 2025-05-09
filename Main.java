3387aaea11c56f633faa3a6974c62c1a9efc98cf
Satyaki7 <dsatyaki956@gmail.com>
2025-05-09 13:24:03 +0530

    Day 3 and 4

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@
 public class Main {
 
     public static void main(String[] args) {
-
         //-------------Fun stuff-----------------------
         // Date dateop = new Date(); <--- 4/5/2025
         // System.out.println(dateop);
@@ -23,5 +22,71 @@ public class Main {
         // System.out.println(Arrays.deepToString(nums)); // Arrays.deepToString() for multidimensional arrays
         // int[][] nums = {{1, 2, 3}, {5, 6, 7}};
         // System.out.println(Arrays.deepToString(nums));
+        //
+        //Constants ----> final <datatype> <variable> = <data>;
+        //
+        // int a = (int) Math.pow(2, 3);
+        // System.out.println(a);
+        //
+        //--------User Input ---------------
+        // Scanner s = new Scanner(System.in); // System.out to print on the terminal
+        // System.out.print("Enter something: ");
+        // int a = s.nextInt(); // next(); for a single word and etc and nextLine(); for the whole line
+        // s.nextLine();
+        // System.out.print("Enter something: ");
+        // String b = s.nextLine().trim();
+        // System.out.println(a + b);
+        //
+        // ------- Conditional Statements ----------
+        /*
+        if (condition){
+            ------
+            --
+        }else if(condition){
+            -----
+            ---        
+        }else{
+            -----        
+        }
+         */
+        //
+        //--------Switch ---------
+        /*
+        switch(arg){
+            case <value of arg>:
+                ----;
+                break;
+            case <value of arg>:
+                ----;
+                break;
+            case <value of arg>:
+                ----;
+                break;
+            default:
+                ----;
+        }
+         */
+        //
+        //--------------Loops-------------
+        /*
+        for (int i = 0;i<n;i++){
+            ------;
+        }
+        int i = something;
+        while(i bla bla){
+            -------;
+        }
+        
+
+        do{
+            -----;
+        }while(condition);
+
+        --------for each function-------------
+        String[] l = {"helo","hello","world"};
+        for(String f: l){
+            System.out.println(f);
+        }
+         */
     }
 }
